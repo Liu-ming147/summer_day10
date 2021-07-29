f66ee938ead003d70ee4f3f626c964041d83b78b
Liu-ming147 <1479763099@qq.com>
2021-07-29 16:26:23 +0800

    提交代码

---
+++ algorithm_10/algorithm_10/在字符串中找出第一个只出现一次的字符.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	char ch[10];
+	int i, j, count;
+	scanf("%s", ch);
+	i = 1;
+	j = 0;
+	count = 1;
+	while (ch[j] != '\0')
+	{
+		while (ch[i] != '\0')
+		{
+			if (j != i)
+			{
+				if (ch[j] == ch[i])
+				{
+					count++;
+					break;
+				}
+			}
+			i++;
+		}
+		if (count == 1)
+		{
+			printf("%c\n", ch[j]);
+			break;
+		}
+		else
+		{
+			count = 1;
+		}
+		j++;
+		i = 0;
+	}
+
+	return 0;
+}
